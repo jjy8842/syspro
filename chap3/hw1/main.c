6eb1ebc2556964f6c2f255aeb1a0c6684eb3db41
jjy8842 <wjdwodbs001@gmail.com>
2024-09-30 22:23:57 +0900

    home

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 #include "copy.h"
 
 char line[MAXLINE];
-char longe[MAXLINE];
+char swap[MAXLINE];
+char str[5][MAXLINE];
 
-main()
+int main()
 {
-	int len;
-	int max;
-	max = 0;
+        for(int i=0; i<5; i++)
+        {
+                if (fgets(line, MAXLINE, stdin) != NULL)
+                {
+                        copy(str[i], line);
+                }
+        }
 
-	for(int i=0;i<=5;i++)
-	{
-		len = strlen(line);
-	}
+        for(int i=0; i<5; i++)
+        {
+                for(int j=i+1; j<5; j++)
+                {
+                        if(strlen(str[i]) < strlen(str[j]))
+                        {
+                                copy(swap, str[i]);
+                                copy(str[i], str[j]);
+                                copy(str[j], swap);
+                        }
+                }
+        }
+        for(int i=0; i<5; i++)
+        {
+                printf("%s", str[i]);
+        }
+        return 0;
 }
